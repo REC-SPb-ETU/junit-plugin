b243360d5f7568dc42f94077abc3ebedd2246e89
osiptsoff <osipczovnikita@gmail.com>
2024-10-29 19:43:23 +0300

    Support new features in frontend

--- src/main/resources/hudson/tasks/test/TestResultProjectAction/junit-trend-config.js
+++ src/main/resources/hudson/tasks/test/TestResultProjectAction/junit-trend-config.js
@@ -1,11 +1,15 @@
 function fillJunit(trendConfiguration, jsonConfiguration) {
   const useBlue = jsonConfiguration['useBlue'];
   trendConfiguration.find('#junit-use-blue').prop('checked', !!useBlue);
+
+  const nodewise = jsonConfiguration['nodewise'];
+  trendConfiguration.find('#junit-nodewise').prop('checked', !!nodewise);
 }
 
 function saveJunit(trendConfiguration) {
   return {
-    'useBlue': trendConfiguration.find('#junit-use-blue').is(':checked')
+    'useBlue': trendConfiguration.find('#junit-use-blue').is(':checked'),
+    'nodewise': trendConfiguration.find('#junit-nodewise').is(':checked')
   };
 }
 
