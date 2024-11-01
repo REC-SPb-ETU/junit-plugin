a7d8515e31c7010046d0e9aa620291768e6f5700
osiptsoff <osipczovnikita@gmail.com>
2024-11-01 13:59:53 +0300

    Change frontend according to backend changes

--- src/main/resources/hudson/tasks/test/TestResultProjectAction/junit-trend-config.js
+++ src/main/resources/hudson/tasks/test/TestResultProjectAction/junit-trend-config.js
@@ -1,15 +1,12 @@
 function fillJunit(trendConfiguration, jsonConfiguration) {
   const useBlue = jsonConfiguration['useBlue'];
   trendConfiguration.find('#junit-use-blue').prop('checked', !!useBlue);
-
-  const nodewise = jsonConfiguration['nodewise'];
-  trendConfiguration.find('#junit-nodewise').prop('checked', !!nodewise);
 }
 
 function saveJunit(trendConfiguration) {
   return {
     'useBlue': trendConfiguration.find('#junit-use-blue').is(':checked'),
-    'nodewise': trendConfiguration.find('#junit-nodewise').is(':checked')
+    'nodeId': trendConfiguration.find('#junit-node-id').prop('value')
   };
 }
 
